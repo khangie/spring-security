e2562b2d0298911b061492d7132e85f05804c7ea
= <khangie@hotmail.com>
2020-11-08 12:35:49 -0800

    Added password encoding

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,16 +1,30 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
-    // This section configures which URLs need to be authenticated or not
+    // Configures which URLs need to be authenticated or not
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests() // Authorize requests
@@ -20,5 +34,20 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated() // Any request must be authenticated with username and password
                 .and()
                 .httpBasic(); // Use basic authentication
+    }
+
+    @Override
+    @Bean
+    // Defines users
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT") // Defined in Spring as ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annaSmithUser
+        );
     }
 }
