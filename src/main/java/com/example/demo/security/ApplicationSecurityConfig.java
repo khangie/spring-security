51a8dfaa691f540eff24e7ca1432ff9db8b67fac
= <khangie@hotmail.com>
2020-11-08 12:19:07 -0800

    configuration

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,7 +1,24 @@
 package com.example.demo.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class ApplicationSecurityConfig {
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    // This section configures which URLs need to be authenticated or not
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests() // Authorize requests
+                .antMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll() // Whitelist all pages that match the previous antMatchers
+                .anyRequest()
+                .authenticated() // Any request must be authenticated with username and password
+                .and()
+                .httpBasic(); // Use basic authentication
+    }
 }
