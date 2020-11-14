24f0e808a0429ed5e7da1ac0008953c7036828c8
= <khangie@hotmail.com>
2020-11-14 14:18:12 -0800

    added csrf

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static com.example.demo.security.ApplicationUserRole.*;
 
@@ -32,7 +33,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     // Configures which URLs need to be authenticated or not
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf().disable() // Disable cross site request forgery for use with Postman
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
                 .authorizeRequests() // Authorize requests
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll() // Whitelist all pages that match the previous antMatchers
                 .antMatchers("/api/**").hasRole(STUDENT.name()) // Requires a STUDENT role to access /api path
